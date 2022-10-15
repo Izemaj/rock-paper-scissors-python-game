5929b20460f4358c1aa578688e7c25887c20c509
Ize Majebi <107319849+Izemaj@users.noreply.github.com>
2022-10-15 16:09:17 +0100

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -25,29 +25,32 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-
+# Create a list of the above play options
 game = [rock,paper,scissors]
 print('Welcome to Rock,Paper,Scissors game!')
 print('Rules of the game:\nRock wins against scissors.\nScissors win against paper.\nPaper wins against rock.\nSame choice is a draw.')
-user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+# Collect the User's Choice
+user_choice = game[int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))]
+# Generate the computer's choice by assigning a random integer
+computer_choice = game[random.randint(0,2)]
 if user_choice < 3:
   print(game[user_choice])
-  computer_choice = random.randint(0,2)
   print(f"Computer chose {computer_choice} !")
   print(game[computer_choice])
-  if game[user_choice] == game[computer_choice]:
+  if user_choice == computer_choice:
     print("It's a draw")
-  elif game[user_choice] == rock and game[computer_choice] == scissors:
+  elif user_choice == rock and computer_choice == scissors:
     print('You won!')
-  elif game[user_choice] == scissors and game[computer_choice] == paper:
+  elif user_choice == scissors and computer_choice == paper:
     print('You won!')
-  elif game[user_choice] == paper and game[computer_choice] == rock:
+  elif user_choice == paper and computer_choice == rock:
     print('You won!')
-  elif game[computer_choice] == rock and game[user_choice] == scissors:
-    print('Computer won!')
-  elif game[computer_choice] == scissors and game[user_choice] == paper:
-    print('Computer won!')
-  elif game[computer_choice] == paper and game[user_choice] == rock:
-    print('Computer won!')
+  elif computer_choice == rock and user_choice == scissors:
+    print('You lose!')
+  elif computer_choice == scissors and user_choice == paper:
+    print('You lose!')
+  elif computer_choice == paper and user_choice == rock:
+    print('You lose!')
 else:
   print('Wrong input! You lose!')
